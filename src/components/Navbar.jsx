2b4a8c01c47f10a07c1005df59f368c84628ce71
PedroItaloQ <peedroitalo2906@gmail.com>
2024-07-13 17:50:37 -0300

    feat: added imagemLogo in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import { Nav, NavLinksContainer, NavLink } from "@/styles/navbarStyled"
+import Image from "next/image";
+import img from "@/assets/Star_Wars_Logo.svg.png"
 
 const Navbar = () => {
     return (
         <Nav>
+            <Image src={img} alt="logoStarWars" width="100" height="100"/>
             <NavLinksContainer>
                 <NavLink href="/" passHref>
                     Home
